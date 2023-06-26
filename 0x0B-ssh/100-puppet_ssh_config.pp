eef496f26f57d8ec8fa5c9d4c35cea79cba48a57
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-06-26 05:22:44 -0700

    changes

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,9 @@
-# puppet config file
-key = ~/.ssh/school
-PasswordAuthentication = 'no'
+# Puppet config file
+file_line { 'disable password':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
+file_line { 'add path':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
+}
