f334033c2a5a4fde973643379c341f9e50abb7ef
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-07-26 06:55:36 -0700

    new

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+
+Python script to export data in the CSV format
+
+"""
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    u_id = argv[1]
+    username = get(url + "users/{}".format(u_id)).json().get("username")
+    todo_list = get(url + "users/{}/todos".format(u_id)).json()
+    with open("{}.csv".format(u_id), "w") as f:
+        for task in todo_list:
+            file.write("'{}','{}','{}','{}'\n".format(u_id, username,
+                       task.get("completed"), task.get("title")))
