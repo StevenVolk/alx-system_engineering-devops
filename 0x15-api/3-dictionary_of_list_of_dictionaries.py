580c530f4f50921237f014e8a684d3ead6a9c6ff
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-07-26 07:32:11 -0700

    new

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -11,9 +11,9 @@ from json import dump
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    users = get(url + "users".json())
+    users = get(url + "users").json()
     with open("todo_all_employees.json", "w") as f:
         dump({user["id"]: [{"task": task["title"],
-             "completed": task["completed"], "username": username}
+             "completed": task["completed"], "username": user["username"]}
               for task in get(url + "users/{}/todos".format(user["id"])).json()]
               for user in users}, f)
