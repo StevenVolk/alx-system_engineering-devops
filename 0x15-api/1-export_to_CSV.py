763e37d749151733deeedddb63929f90e02f6fc8
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-07-26 06:59:40 -0700

    new

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
     todo_list = get(url + "users/{}/todos".format(u_id)).json()
     with open("{}.csv".format(u_id), "w") as f:
         for task in todo_list:
-            file.write("'{}','{}','{}','{}'\n".format(u_id, username,
+            f.write("'{}','{}','{}','{}'\n".format(u_id, username,
                        task.get("completed"), task.get("title")))
