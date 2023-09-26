f2791a2cfd4b97791fde6693f108c0c4432fa8b3
Adedimeji Ojerinde <36421837+StevenVolk@users.noreply.github.com>
2023-09-25 21:17:19 -0400

    Delete 0x0B-ssh directory

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++
@@ -1,9 +0,0 @@
-# Puppet config file
-file_line { 'disable password':
-  path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication no',
-}
-file_line { 'add path':
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/school',
-}
