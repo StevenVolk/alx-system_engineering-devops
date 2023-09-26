b395248f0cdb158fa69abf1a2c5e31b0f7f9d10e
Adedimeji Ojerinde <36421837+StevenVolk@users.noreply.github.com>
2023-09-25 21:17:09 -0400

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-#  create a file - school in /tmp
-file { '/tmp/school':
-  ensure  => file,
-  path    => '/tmp/school',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0744',
-  content => 'I love Puppet',
-}
